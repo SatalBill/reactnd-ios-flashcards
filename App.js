94d1c77cf9b5b6232a92e3c4710a8e45b475e567
sujinleeme <sujinlee.me@gmail.com>
2018-03-01 23:52:02 +0900

    Config Eslint

--- App.js
+++ App.js
@@ -1,23 +1,21 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+/* eslint-disable comma-dangle */
+import React from "react"
+import { StyleSheet, Text, View } from "react-native"
 
 export default class App extends React.Component {
-  render() {
+  render () {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+      <View style={ styles.container }>
       </View>
-    );
+    )
   }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  }
+})
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # React Native ios Flash Cards
 A project for udacity nanodegree final assignment
+
+* `yarn run ios`
