e11ec02a963bf96b9b51e6c74284890ff8e749e6
sujinleeme <sujinlee.me@gmail.com>
2018-03-02 15:50:56 +0900

    Create Sample Deck List

--- Storage.js
+++ Storage.js
@@ -1,24 +1,26 @@
-export const Storage = {
-	React: {
-		title: "React",
-		questions: [
-			{
-				question: "What is React?",
-				answer: "A library for managing user interfaces"
-			},
-			{
-				question: "Where do you make Ajax requests in React?",
-				answer: "The componentDidMount lifecycle event"
-			}
-		]
-	},
-	JavaScript: {
-		title: "JavaScript",
-		questions: [
-			{
-				question: "What is a closure?",
-				answer: "The combination of a function and the lexical environment within which that function was declared."
-			}
-		]
-	}
+export const storage = {
+  React:      {
+    id: 0,
+    title:     "React",
+    questions: [
+      {
+        question: "What is React?",
+        answer:   "A library for managing user interfaces"
+      },
+      {
+        question: "Where do you make Ajax requests in React?",
+        answer:   "The componentDidMount lifecycle event"
+      }
+    ]
+  },
+  JavaScript: {
+    id: 1,
+    title:     "JavaScript",
+    questions: [
+      {
+        question: "What is a closure?",
+        answer:   "The combination of a function and the lexical environment within which that function was declared."
+      }
+    ]
+  }
 }
