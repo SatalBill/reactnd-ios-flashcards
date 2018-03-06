b5ba5250ca83d8b821323cb131b40314bcd6f033
sujinleeme <sujinlee.me@gmail.com>
2018-03-06 17:39:44 +0900

    Create helper api funcs

--- utils/api.js
+++ utils/api.js
@@ -1,5 +1,23 @@
 import { AsyncStorage } from "react-native"
 
+export const getDecks = () => {
+
+}
+
+export const getDeck = ({key}) => {
+  /*
+  Take in a single id argument and return the deck associated with that id
+   */
+}
+
+export const saveDeckTitle = ({title}) => {
+  /* take in a sigle title argument and add it to the decks
+ */
+}
 
 
-
+export const addCardToDeck = ({title, cards}) => {
+  /*
+take two arguments title and card and will add the card to the list deck with the associated title
+ */
+}
