71ec833861505f988e0e3e7e262e314fb6747500
sujinleeme <sujinlee.me@gmail.com>
2018-03-07 22:17:53 +0900

    Add react-logger

--- config/store.js
+++ config/store.js
@@ -1,10 +1,12 @@
 import { compose, createStore, applyMiddleware } from "redux"
 import thunk from "redux-thunk"
 import { AsyncStorage } from "react-native"
-
+import { createLogger } from 'redux-logger'
 import reducers from "../reducers"
 
-const middleware = [thunk]
+const logger = createLogger()
+const middleware = [thunk, logger]
+
 // eslint-disable-next-line no-underscore-dangle
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 
