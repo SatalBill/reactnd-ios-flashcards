8d449405dd242f1a6bc475505af5198232bb4708
sujinleeme <sujinlee.me@gmail.com>
2018-04-08 01:08:28 +0900

    feat: Show error message when deck title is already existed

--- components/NewDeck/NewDeck.js
+++ components/NewDeck/NewDeck.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react"
 import PropTypes from "prop-types"
 import { StyleSheet, View, ScrollView, TouchableOpacity } from "react-native"
-import { Header, Button, Input } from "react-native-elements"
+import { Text, Header, Button, Input } from "react-native-elements"
 import styles from "./styles"
 import { GoBackIcon } from "../NavIcons"
 
@@ -11,36 +11,57 @@ export default class NewDeck extends Component {
     super(props)
     this.state = {
       title: "",
-      _isFilled: false
+      _isFilled: false,
+      _isError: false
     }
   }
 
   onTitleChange = (title) => {
     const _isFilled = title.length > 0
     this.setState({title, _isFilled})
+    this.initErrorMessage(_isFilled)
+  }
+
+  initErrorMessage = (_isFilled) => {
+    if (!_isFilled) {
+      this.setState({_isError: false})
+    }
   }
 
   submit = () => {
-    const {title} = this.state
-    if (this.state._isFilled) {
-      // save to db
-      this.props.addDeck({title})
-      return this.props.goToBack()
+    const {title, _isFilled} = this.state
+    const {list} = this.props
+    if (_isFilled) {
+      if (list[title]) {
+        this.setState({_isError: true})
+      }
+      else {
+        this.props.addDeck({title})
+        return this.props.goToBack()
+      }
     }
   }
 
   render () {
-    const buttonStyle = this.state.title.length === 0 ? styles.inactivedButton : styles.activedButton
+    const {title, _isError} = this.state
+    const buttonStyle = title.length === 0 ? styles.inactivedButton : styles.activedButton
+    //const errorMsg = _isError ? "Already created" : ""
 
+    // const errorMessage = this.state
     return (
-      <View >
+      <View>
         <Header
           leftComponent={<GoBackIcon/>}
         />
-        <View >
+        <View>
           <Input
+
             placeholder="Write Deck Title"
             onChangeText={this.onTitleChange}
+            errorStyle={{color: "red"}}
+          />
+          <ErrorMessage
+            error={_isError}
           />
           <Button
             text="SUBMIT"
@@ -56,3 +77,8 @@ export default class NewDeck extends Component {
     )
   }
 }
+
+const ErrorMessage = ({error}) =>
+  <Text>
+    {error? "Already created" : ""}
+  </Text>
