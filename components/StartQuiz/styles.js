7251f0ed47b0dda4e45e35f153410116a8190224
sujinleeme <sujinlee.me@gmail.com>
2018-04-16 20:48:11 +0900

    style: Move style code to style.js

--- components/StartQuiz/styles.js
+++ components/StartQuiz/styles.js
@@ -3,37 +3,91 @@ import { StyleSheet } from "react-native"
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center"
+    flexDirection: "column",
+    backgroundColor: "#f2f2f2",
   },
-  profile: {
+  content: {
+    flex: 5,
+    alignItems: "center",
+    justifyContent: "center",
+
+  },
+  card: {
+    width: 320,
+    height: 470,
+    padding: 14,
+    backgroundColor: "#FE474C",
+    borderRadius: 5,
+    shadowColor: "rgba(0,0,0,0.5)",
+    shadowOffset: {
+      width: 0,
+      height: 1
+    },
+    shadowOpacity: 0.5,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  card1: {
+    backgroundColor: "#FE474C",
+  },
+  card2: {
+    backgroundColor: "#FEB12C",
+  },
+  label: {
+    textAlign: "center",
+    fontSize: 30,
+
+    // fontFamily: "System",
+
+    color: "#fff",
+    backgroundColor: "transparent"
+  },
+  footer: {
     flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  buttonContainer: {
+    width: 220,
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  button: {
+    shadowColor: "rgba(0,0,0,0.3)",
+    shadowOffset: {
+      width: 0,
+      height: 1
+    },
+    shadowOpacity: 0.5,
     backgroundColor: "#fff",
     alignItems: "center",
-    justifyContent: "center"
+    justifyContent: "center",
+    zIndex: 0,
   },
-  decklist: {
-    flex: 2,
+  orange: {
+    width: 55,
+    height: 55,
+    borderWidth: 6,
+    borderColor: "rgb(246,190,66)",
+    borderWidth: 4,
+    borderRadius: 55,
+    marginTop: -15
+  },
+  green: {
+    width: 75,
+    height: 75,
     backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center"
+    borderRadius: 75,
+    borderWidth: 6,
+    borderColor: "#01df8a",
   },
-  activedButton: {
-    backgroundColor: "rgba(92, 99,216, 1)",
-    width: 300,
-    height: 45,
-    borderColor: "transparent",
-    borderWidth: 0,
-    borderRadius: 5
-  },
-  inactivedButton: {
-    backgroundColor: "gray",
-    width: 300,
-    height: 45,
-    borderColor: "transparent",
-    borderWidth: 0,
-    borderRadius: 5
+  red: {
+    width: 75,
+    height: 75,
+    backgroundColor: "#fff",
+    borderRadius: 75,
+    borderWidth: 6,
+    borderColor: "#fd267d",
   }
 })
 
