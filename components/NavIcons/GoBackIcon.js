34d134b1ea200bf0816e20b540b0ef868e18ef16
sujinleeme <sujinlee.me@gmail.com>
2018-05-03 21:24:06 +0900

    refactor: Make GoBackIcon more types

--- components/NavIcons/GoBackIcon.js
+++ components/NavIcons/GoBackIcon.js
@@ -3,14 +3,36 @@ import { TouchableOpacity } from "react-native"
 import { Icon } from "react-native-elements"
 import { connect } from "react-redux"
 
-import { goToBack } from "../../actions"
+import { goToBack, openHome } from "../../actions"
 
 const GoBackIcon = (props) => {
+  let status
+  switch (props.type) {
+    case "DEFAULT":
+      status = {
+        icon: "arrow-left",
+        nextNav: props.goToBack
+      }
+      break
+    case "HOME":
+      status = {
+        icon: "list",
+        nextNav: props.openHome
+      }
+      break
+    default:
+      break
+
+  }
+  console.log(status)
+
   return (
-    <TouchableOpacity onPress={props.goToBack}>
+    <TouchableOpacity
+      onPress={status.nextNav}
+    >
       <Icon
         size={23}
-        name="arrow-left"
+        name={status.icon}
         type="feather"/>
     </TouchableOpacity>
   )
@@ -24,6 +46,9 @@ const mapDispatchToProps = dispatch => {
   return {
     goToBack: () => {
       dispatch(goToBack())
+    },
+    openHome: () => {
+      dispatch(openHome())
     }
   }
 }
