c48b50172d549f756555ff54fc5284136f3465f3
sujinleeme <sujinlee.me@gmail.com>
2018-05-03 21:29:32 +0900

    refactor: Add title, leftNavType default props value

--- components/MainDeckHeader/MainDeckHeader.js
+++ components/MainDeckHeader/MainDeckHeader.js
@@ -1,15 +1,20 @@
 import React, { Component } from "react"
-import { View, Header } from "react-native-elements"
+import { Header } from "react-native-elements"
 import { GoBackIcon } from "../NavIcons"
 import styles from "./styles"
 
-const MainDeckHeader = ({title}) =>
-  <Header
-    statusBarProps={{barStyle: "light-content", translucent: true }}
-    leftComponent={<GoBackIcon/>}
-    centerComponent={{text: title, style: styles.title}}
-    innerContainerStyles={styles.innerContainer}
-    outerContainerStyles={styles.outerContainer}
-  />
+const MainDeckHeader = ({title = "", leftNavType = "DEFAULT"}) => {
+  return (
+    <Header
+      statusBarProps={{barStyle: "light-content", translucent: true}}
+      leftComponent={
+        < GoBackIcon
+          type={leftNavType}
+        />}
+      centerComponent={{text: title, style: styles.title}}
+      innerContainerStyles={styles.innerContainer}
+      outerContainerStyles={styles.outerContainer}
+    />)
+}
 
 export default MainDeckHeader
